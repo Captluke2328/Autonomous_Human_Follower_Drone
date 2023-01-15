136c493465ce0704548a2634784cb8c27d44740b
Captluke2328 <lukasjohnny2328@gmail.com>
2023-01-15 23:25:20 +0800

    + added video record

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-#mkdir "/home/jlukas/Desktop/My_Project/new_usb"
+mkdir "/home/jlukas/Desktop/My_Project/main_log"
 echo Running at boot
 python3 /home/jlukas/Desktop/My_Project/Autonomous_Human_Follower_Drone/main.py
